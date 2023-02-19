f2bf89a3bee32b5b6149c569774f962ae3417ee6
Paweł Czapla <pawel.czapla@rednt.eu>
2023-02-19 20:49:59 +0100

    Zmiana pól klasy ImportedObject we właściwości

--- ConsoleApp/DataReader.cs
+++ ConsoleApp/DataReader.cs
@@ -86,18 +86,13 @@
 
     class ImportedObject : ImportedObjectBaseClass
     {
-        public string Schema;
-
-        public string ParentName;
-        public string ParentType
-        {
-            get; set;
-        }
-
+        public string Schema { get; set; }
+        public string ParentName { get; set; }
+        public string ParentType { get; set; }
         public string DataType { get; set; }
         public string IsNullable { get; set; }
 
-        public int NumberOfChildren;
+        public int NumberOfChildren { get; set; }
     }
 
     class ImportedObjectBaseClass
